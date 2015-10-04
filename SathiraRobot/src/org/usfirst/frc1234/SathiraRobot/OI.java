b33942a95e0b4311c4a01e48d4f8d8661ddf9554
Sathira Katugaha <sathirakatugaha@gmail.com>
2015-10-04 10:41:55 -0700

    System.out.println changed to LOGGING

--- SathiraRobot/src/org/usfirst/frc1234/SathiraRobot/OI.java
+++ SathiraRobot/src/org/usfirst/frc1234/SathiraRobot/OI.java
@@ -22,7 +22,7 @@ import org.usfirst.frc1234.SathiraRobot.commands.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-
+// Yay
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
